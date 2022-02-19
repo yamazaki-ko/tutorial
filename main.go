ef84cb4f7be54142e68a45d6b61774d1945b499f
yamazaki-ko <65061306+yamazaki-ko@users.noreply.github.com>
2022-02-19 13:19:32 +0900

    Revert "issue3"

--- main.go
+++ main.go
@@ -2,54 +2,36 @@ package main
 
 func main() {
 	// #4-1
-	// echo
+	// echo インストール
 
 	// #4-2
-	// アプリディレクトリ
+	// server package
 
 	// #4-3
+	// Hello World
+
+	// #4-4
 	// GOモジュール
 	// パッケージ管理ツール
 	// go mod init
 	// go mod tidy
 
-	// #4-4
-	// echo インストール
-
 	// #4-5
 	// クイックスタート
-	// Hello World
+	// go run main.go
 
 	// #4-6
-	// GETリクエスト：リソース取得
-	// URLの ? の後にパラメータを付けてデータを送信する。
-
-	// POSTリクエスト：リソース追加、置換
-	// 送信するデータはメッセージボディに入れる＝見えない
-	// 機密性の高いデータ
-	// 大量のデータ（リクエストパラメータに収まらない）
-
-	// PUTリクエスト：リソース更新
-	// あまり使わない
-
-	// DELETE：リソース削除
-	// あまり使わない
-
-	// POSTで十分代用可能
-	// htmlのformがPUT/DELETEをサポートしていない
-
-	// #4-7
 	// Path Parameters
 	// Query Parameters
 
-	// #4-8
+	// #4-7
 	// Templates
-	// public/views/hello.html
+
+	// #4-8
+	// ハンドリング整理
 
 	// #4-9
-	// {{define "hello"}}を使い回す
 
 	// #4-10
-	// css使ってみる
-	//
+
 }
